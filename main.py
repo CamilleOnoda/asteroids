0413e224c73aa9da1dedce96d035e659ca78b469
CamilleOnoda <info@camilleonoda.com>
2025-02-06 11:45:23 +0900

    initialize pygame and display the screen

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH,ASTEROID_KINDS,\
+ASTEROID_MAX_RADIUS,ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE
 
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 
 
