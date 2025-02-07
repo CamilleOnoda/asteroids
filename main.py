d9fd81f1cf34e8a6eb05ab3dabd28aad7e37cb56
CamilleOnoda <info@camilleonoda.com>
2025-02-07 22:47:04 +0900

    create a new group to contain all of the asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
 
 
 def main():
@@ -10,7 +11,9 @@ def main():
 
     updatable  = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
