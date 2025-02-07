097b8843ab54f30783422cd1c2e4851eee856d12
CamilleOnoda <info@camilleonoda.com>
2025-02-07 14:53:25 +0900

    rearrange elements outside and inside the game loop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from constants import SCREEN_HEIGHT,SCREEN_WIDTH,ASTEROID_KINDS,\
-ASTEROID_MAX_RADIUS,ASTEROID_MIN_RADIUS,ASTEROID_SPAWN_RATE
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from player import Player
 
 
@@ -9,23 +8,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill((0,0,0))
-
-        x = SCREEN_WIDTH / 2
-        y = SCREEN_HEIGHT / 2
-        player = Player(x,y)
-        player.draw(screen)
-        
-        pygame.display.flip()
-        clock = pygame.time.Clock()
-        dt = 0
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            delta_time = clock.tick(60)
-            dt = delta_time / 1000
+            
+        dt = clock.tick(60) / 1000.0
+        player.update(dt)
+        screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()   
 
 
 if __name__ == "__main__":
