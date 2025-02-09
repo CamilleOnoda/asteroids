97a1aca250a606bbd83e60fc42cd28062df885bf
CamilleOnoda <info@camilleonoda.com>
2025-02-09 22:50:28 +0900

    correct the initialization of the class

--- shot.py
+++ shot.py
@@ -3,14 +3,11 @@ from circleshape import CircleShape
 from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-    def __init__(self, position, velocity):
-        super().__init__(position.x, position.y, SHOT_RADIUS)
-        self.position = pygame.Vector2(position)
-        self.velocity = pygame.Vector2(velocity)
-        self.radius = SHOT_RADIUS
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255,255,255), self.position, SHOT_RADIUS, width=2)
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
