658d6c166c52da2c1356283f6cb11af6ab2afc7c
CamilleOnoda <info@camilleonoda.com>
2025-02-09 15:20:17 +0900

    set up a new group to contain all the shots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
+from shot import Shot
 import sys
 
 
@@ -15,9 +16,11 @@ def main():
     updatable  = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -42,6 +45,7 @@ def main():
 
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()   
 
 
